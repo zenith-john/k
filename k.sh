dfae61acd5fff29e7abf797fd83eecb0525a148b
Zhang Nantao <zenith-john@foxmail.com>
2019-04-25 22:40:15 +0800

    Make k.sh work in my Ubuntu Chinese environment.

--- k.sh
+++ k.sh
@@ -294,7 +294,7 @@ k () {
     do
       statvar="stats_$i"
       typeset -A $statvar
-      zstat -H $statvar -Lsn -F "%s^%d^%b^%H:%M^%Y" -- "$fn"  # use lstat, render mode/uid/gid to strings
+      zstat -H $statvar -Lsn -F "%s" -- "$fn"  # use lstat, render mode/uid/gid to strings
       STATS_PARAMS_LIST+=($statvar)
       i+=1
     done
@@ -359,7 +359,8 @@ k () {
                OWNER="${sv[uid]}"
                GROUP="${sv[gid]}"
             FILESIZE="${sv[size]}"
-                DATE=(${(s:^:)sv[mtime]}) # Split date on ^
+                TEMP=$(date +"%s^%d^%b^%H:%M^%Y" -d @${mtime})
+                DATE=(${(s:^:)TEMP}) # Split date on ^
                 NAME="${sv[name]}"
       SYMLINK_TARGET="${sv[link]}"
 
